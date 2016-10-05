e1af87896509506b2c710044e4c0f9ed7ff90d0a
okamuroshogo <okamuro-shogo@kayac.com>
2016-10-05 20:31:37 +0900

    update capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,25 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value 
 
+set :puma_user, fetch(:user)
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix:///tmp/puma.socket"    #accept array for multi-bind
+set :puma_default_control_app, "unix:///tmp/sockets/pumactl.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'development'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
+set :puma_preload_app, false
+set :puma_plugins, []  #accept array of plugins
+set :nginx_use_ssl, false
+
 # デプロイ前に実行する必要がある。
 desc 'execute before deploy'
 task :db_create do
