18ef31dd96e15b277934527d5138ccded3a76a86
okamuroshogo <okamuro-shogo@kayac.com>
2016-10-06 17:44:37 +0900

    タブのズレを修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,12 +1,12 @@
 class TopController < BaseController
-	require 'twitter'
+  require 'twitter'
 
-	#index
+  #index
   def index
 
   end
 
-	#twitterの認証完了CallBack
+  #twitterの認証完了CallBack
   def callback
     auth = request.env['omniauth.auth']
     user = User.find_by_uid(auth['uid']) || User.create_with_omniauth(auth)
@@ -21,11 +21,11 @@ class TopController < BaseController
     redirect_to root_path
   end
 
-private	
-		# 自分のタイムラインを取得
-		def getTimeLine
-			Config::CLIENT.home_timeline.each do |tweet|
+  private	
+    # 自分のタイムラインを取得
+    def getTimeLine
+	    Config::CLIENT.home_timeline.each do |tweet|
 			  puts tweet.full_text
-			end
-		end
+	    end
+	  end
 end
