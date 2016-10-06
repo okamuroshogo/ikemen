7f2a53c24c10f8af97123b225d3ef3f361b5881a
okamuroshogo <okamuro-shogo@kayac.com>
2016-10-06 18:42:35 +0900

    #9 SessionControllerを追加

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,25 @@
+class SessionController < ApplicationController
+  
+  #twitterの認証完了CallBack
+  def callback
+    auth = request.env['omniauth.auth']
+    user = User.find_by(uid: auth['uid']) || User.create_with_omniauth(auth)
+    getTimeLine
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+
+  #ログアウト処理
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+    # 自分のタイムラインを取得
+    def getTimeLine
+      Config::CLIENT.home_timeline.each do |tweet|
+        puts tweet.full_text
+      end
+    end
+end
