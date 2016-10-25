cf626f738a96095e7b5fbcbae986fae232cf6219
okamuroshogo <okamuro-shogo@kayac.com>
2016-10-25 18:22:45 +0900

    testのテンプレを書いてみた

--- spec/ikemen/analyze_spec.rb
+++ spec/ikemen/analyze_spec.rb
@@ -2,11 +2,31 @@ require 'spec_helper'
 
 
 describe Morpheme do
-  before(:each) { @sample = Morpheme.new(nil) }
+  before(:each) { @mor = Morpheme.new(nil) }
 
-  context 'privateメソッドを呼び出す場合' do
-    subject { @sample.send(:tweet_replacement,"aaaaaaaa").should == "aaaaaaaa" }
-    # Object#sendはレシーバの持っているメソッドを呼び出す
+  context '正規表現のテスト　何も起きない' do
+    subject { @mor.send(:tweet_replacement,"ぼくは、ごはんを、たべる").should == "ぼくは、ごはんを、たべる" }
+    it { should == true }
+  end
+  context '正規表現のテスト スペース連続' do
+    subject { @mor.send(:tweet_replacement,"ぼくは　　ごはんをたべる").should == "ぼくはごはんをたべる" }
+    it { should == true }
+  end
+  context '正規表現のテスト ２回のスペース' do
+    subject { @mor.send(:tweet_replacement,"ぼくは　ごはんを　たべる").should == "ぼくはごはんをたべる" }
+    it { should == true }
+  end
+  context '正規表現のテスト 複数回のスペース' do
+    subject { @mor.send(:tweet_replacement,"ぼ　く　は　ご　は　ん").should == "ぼくはごはん" }
+    it { should == true }
+  end
+  context '正規表現のテスト tabが含まれる場合' do
+    subject { @sample.send(:tweet_replacement,"").should == "" }
+    it { should == true }
+  end
+  context '正規表現のテスト unicodeが含まれる場合' do
+    subject { @sample.send(:tweet_replacement,"").should == "" }
     it { should == true }
   end
 end
+
